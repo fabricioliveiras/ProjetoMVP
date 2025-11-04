7e5b15da798903c73f1449cb9198a0bbe0251443
Fabricio Oliveira <104277639+fabricioliveiras@users.noreply.github.com>
2025-11-04 16:17:19 -0300

    Refactor photo upload event listeners

--- js/main-p.js
+++ js/main-p.js
@@ -1,17 +1,17 @@
 const photoDiv = document.getElementById('profilePhoto');
-    const photoInput = document.getElementById('photoInput');
+   const photoInput = document.getElementById('photoInput');
 
-    photoDiv.addEventListener('click', () => {
-      photoInput.click();
-    });
+   photoDiv.addEventListener('click', () => {
+     photoInput.click();
+   });
 
-    photoInput.addEventListener('change', (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-          photoDiv.innerHTML = `<img src="${e.target.result}" alt="Foto de perfil">`;
-        };
-        reader.readAsDataURL(file);
+   photoInput.addEventListener('change', (event) => {
+     const file = event.target.files[0];
+     if (file) {
+       const reader = new FileReader();
+       reader.onload = function(e) {
+         photoDiv.innerHTML = `<img src="${e.target.result}" alt="Foto de perfil">`;
+     };
+      reader.readAsDataURL(file);
       }
-    });
+});
